fab1f25c8fe7e0d69dccae20295237dc474e0f52
Sakari <gksakari@163.com>
2024-01-29 15:28:49 +0800

    feat: im-select

---
+++ lua/plugins/im-select.lua
@@ -0,0 +1,17 @@
+return {
+  "brglng/vim-im-select",
+  config = function()
+    require("plugins.im-select")
+  end,
+  cmd = "ImSelectEnable",
+  keys = {
+    { "<leader>um", "<cmd>ImSelectEnable<cr>", desc = "Enable Im-Select" },
+    { "<leader>uM", "<cmd>ImSelectDisable<cr>", desc = "Disable Im-Select" },
+  },
+  init = function()
+    vim.g.im_select_command = "D:/Tools/im-select.exe"
+    vim.g.im_select_default = "1033"
+    vim.g.im_select_enable_focus_events = 0
+    vim.g.im_select_enable_cmd_line = 0
+  end,
+}
