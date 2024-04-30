118ba72c952fcad1a817eac7868ede94ac878a26
Sakari <gksakari@163.com>
2024-04-30 23:51:25 +0800

    fix: markdown转pdf时取消边框

--- lua/plugins/markdown-previewer.lua
+++ lua/plugins/markdown-previewer.lua
@@ -5,13 +5,13 @@ return {
     keys = { { "<leader>cp", "<cmd>MarkdownPreviewToggle<cr>" } },
     lazy = false,
     build = "cd app && npm install && git restore .",
+    ft = { "markdown" }, -- build = "cd app && yarn install",
     init = function()
+      -- vim.g.mkdp_auto_start = 1
       vim.g.mkdp_filetypes = { "markdown" }
       vim.g.mkdp_theme = "dark"
-    end,
-    ft = { "markdown" }, -- build = "cd app && yarn install",
-    -- vim.g.mkdp_auto_start = 1
-    config = function()
+      vim.g.mkdp_preview_options = { ["disable_filename"] = 1 }
+      vim.g.mkdp_markdown_css = "C:/Users/Sakarin/AppData/Local/nvim/css/markdown.css"
       -- vim.g.mkdp_browser = "C:/Program Files/Google/Chrome/Application/chrome.exe"
     end,
   },
