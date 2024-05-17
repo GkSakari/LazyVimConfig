6396f71625ad18401795083a09e1707a308dabc9
Sakari <gksakari@163.com>
2024-05-17 18:08:29 +0800

    feat: determine os

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -111,15 +111,18 @@ vim.g.sqlite_clib_path = "C:/Users/Sakarin/AppData/Local/nvim/data/sqlite3.dll"
 vim.g.lazyvim_python_lsp = "pyright"
 
 -- enable powershell
-local powershell_options = {
-  shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-  shellquote = "",
-  shellxquote = "",
-}
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+if is_windows then
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
 
-for option, value in pairs(powershell_options) do
-  vim.opt[option] = value
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
 end
