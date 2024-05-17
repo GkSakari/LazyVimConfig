5b787893daa7cc2c60a1a06cb5862f3c9c483a01
Sakari <gksakari@163.com>
2024-05-17 13:40:43 +0800

    feat: chinese for flash

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "rainzm/flash-zh.nvim",
+    enabled = true,
+    event = "VeryLazy",
+    dependencies = {
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      keys = { { "s", false } },
+      opts = {
+        highlight = {
+          backdrop = true,
+          matches = true,
+        },
+        modes = {
+          search = { enabled = true },
+        },
+      },
+    },
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash-zh").jump({
+            chinese_only = false,
+          })
+        end,
+        desc = "Flash between Chinese",
+      },
+    },
+  },
+}
--- test.md
+++ test.md
@@ -107,6 +107,8 @@ immediately by a digit. Thus, $20,000 and $30,000 won’t parse as math. If for 
 reason you need to enclose text in literal $ characters, backslash-escape them and
 they won’t be treated as math delimiters.
 
+\$\$12
+
 ### chart and mermaid
 
 ```chart
