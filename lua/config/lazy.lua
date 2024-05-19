f5b6dcaa7c6d7a2d12c4a337592a5a739c029110
Sakari <gksakari@163.com>
2024-05-20 00:16:55 +0800

    feat: debug python

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
     { import = "plugins.ui" },
-    { import = "plugins.dap" },
+    -- { import = "plugins.dap" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
---
+++ lua/plugins/python_debug.lua
@@ -0,0 +1,29 @@
+-- mason.nvim integration
+return {
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = "mason.nvim",
+    cmd = { "DapInstall", "DapUninstall" },
+    opts = {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = false,
+    },
+  },
+  {
+    -- "mfussenegger/nvim-dap",
+    -- optional = true,
+    -- require("dap.ext.vscode").load_launchjs(),
+    -- dependencies = {
+    "mfussenegger/nvim-dap-python",
+      -- stylua: ignore
+      -- keys = {
+      --   { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+      --   { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+      -- },
+    config = function()
+      require("dap-python").setup("python.exe")
+    end,
+    -- },
+  },
+}
