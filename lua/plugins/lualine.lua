2085415ee30a58580a71fd034fc34df6f4ba20ba
Sakari <gksakari@163.com>
2025-03-01 14:29:36 +0800

    feat: cmp虎码

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,24 @@ return {
         { "progress", separator = " ", padding = { left = 0, right = 0 } },
         { "location", padding = { left = 0, right = 1 } },
       },
+      lualine_x = {
+        {
+          function()
+            return ""
+          end,
+          color = function()
+            return { fg = require("cmp_im").getChineseSymbolStatus() and "#A7C080" or "grey" }
+          end,
+        },
+        {
+          function()
+            return "虎"
+          end,
+          color = function()
+            return { fg = require("cmp_im").getStatus() and "#A7C080" or "grey" }
+          end,
+        },
+      },
     },
   },
 }
