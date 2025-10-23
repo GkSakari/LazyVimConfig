43e6a348c5115401c89353ca034cec2a99a849de
Sakari <gksakari@163.com>
2025-10-24 00:34:16 +0800

    feat: markdown
    尝试了几个md预览插件，目前选择使用markview和markdown-preview

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,12 @@
+-- For `plugins/markview.lua` users.
+return {
+  enabled = true,
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+
+  -- For blink.cmp's completion
+  -- source
+  -- dependencies = {
+  --     "saghen/blink.cmp"
+  -- },
+}
